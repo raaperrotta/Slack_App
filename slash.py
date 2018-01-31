e889395144a12de4a5ced8872ce9d1ccc89b05b4
Robert Perrotta <raaperrotta@gmail.com>
2018-01-31 16:40:33 -0500

    Added per_player table creation code to slash commands

--- slash.py
+++ slash.py
@@ -13,11 +13,36 @@ def test():
 
 @route('/per_player', method="post")
 def per_player():
-    postdata = request.forms.get("text")
-    message = 'Hello, World!'
-    package = {"response_type": "in_channel", "text": message}
     response.content_type = 'application/json'
-    return package
+    postdata = request.forms.get("text")
+
+
+    try:
+        # Separate list of numbers by spaces
+        nums = command.split()
+        nums = [int(num) for num in nums]
+
+        message = '```\n'
+        w = 9  # column width in characters
+        message += ' ' * w  # add empty top left corner
+        # Add column headers with number of points
+        for num in nums:
+            message += f'{num:>{w},.0f}'
+        for num_players in range(2, 10):
+            message += f'\n{num_players:>{w}d}'
+            for num in nums:
+                per_player = (num + num_players - 1) // num_players  # ceil
+                message += f'{per_player:>{w},.0f}'
+        message += '\n```'
+
+        message = 'Hello, World!'
+        package = {"response_type": "in_channel", "text": message}
+    except Exception as err:
+        log.debug(f'Got error while parsing command: {err}')
+        message = f'Could not parse your input. Was it just numbers separated by whitespace?\n(Error: {err})'
+        package = {"response_type": "ephemeral", "text": message}
+    finally:
+        return package
 
 if __name__ == '__main__':
 
